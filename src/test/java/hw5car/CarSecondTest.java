b9d5425b503cfe0ffd0048063d4c56b543b8665d
Andrey Burban <burban.andrey@gmail.com>
2023-02-28 13:46:51 +0200

    Setting up Maven profiles

---
+++ src/test/java/hw5car/CarSecondTest.java
@@ -0,0 +1,17 @@
+package hw5car;
+
+import org.testng.annotations.Test;
+
+public class CarSecondTest {
+
+        @Test
+        public void test1() {
+            System.out.println("First Method");
+    }
+
+        @Test
+        public void test2() {
+            System.out.println("Second Method");
+        }
+}
+
--- src/test/java/hw5car/CarTest.java
+++ src/test/java/hw5car/CarTest.java
@@ -6,13 +6,19 @@ public class CarTest {
 
         @Test
         public void test1() {
-        Car rav4 = new Car("Toyota Rav4", "Orange");
-        Car bmw = new Car("BMW X5", "Blue", 5.0, 280);
-        Car ford = new Car("Ford Focus", "Yellow", 2.0);
-
-        rav4.process(rav4);
-        bmw.processColor(bmw.carChangeColor("Gold"));
-        ford.processColor(ford.carChangeColor("White"));
+                Car rav4 = new Car("Toyota Rav4", "Orange");
+                rav4.process(rav4);
     }
+        @Test
+        public void test2() {
+                Car bmw = new Car("BMW X5", "Blue", 5.0, 280);
+                bmw.processColor(bmw.carChangeColor("Gold"));
+        }
+
+        @Test
+        public void test3() {
+                Car ford = new Car("Ford Focus", "Yellow", 2.0);
+                ford.processColor(ford.carChangeColor("White"));
+        }
 }
 
