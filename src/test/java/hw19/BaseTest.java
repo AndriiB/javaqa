6aa81e28a519db739cf01630f3a76281cc4188d5
Andrey Burban <burban.andrey@gmail.com>
2023-03-28 14:41:02 +0300

    Design patterns of shopping project

--- src/test/java/hw19/BaseTest.java
+++ src/test/java/hw19/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
@@ -23,10 +24,10 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-//    @AfterClass
-//    public void closeBrowser() {
-//        driver.quit();
-//    }
+    @AfterClass
+    public void closeBrowser() {
+        driver.quit();
+    }
 
     public HomePage launchRozetka() {
         driver.get("https://rozetka.com.ua/ua/");
